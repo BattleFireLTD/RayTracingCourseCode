61c43f65433dd76560b35ec9556e3164ed046d9a
BattleFireOpen <open@battlefire.cn>
2020-09-30 15:49:04 +0800

    逐像素刷新画面
    Signed-off-by: BattleFireOpen <open@battlefire.cn>

--- Scene.cpp
+++ Scene.cpp
@@ -3,12 +3,14 @@
 #include"Vector3.h"
 #include"Camera.h"
 #include"Ray.h"
+#include<Windows.h>
+#pragma comment(lib,"winmm.lib")
+static int sTotalPixelCount = 0;
+static int sViewportWidth = 0, sViewportHeight = 0;
 void Init(int width, int height) {
-	for (int y = 0; y < height; ++y) {
-		for (int x = 0; x < width; x++) {
-			SetColor(x, y, 255, 0, 0, 255);
-		}
-	}
+	sTotalPixelCount = width * height;
+	sViewportWidth = width;
+	sViewportHeight = height;
 	Vector3 v(1.0f,1.0f,0.0f);
 	v.Normalize();
 	Vector3 v2 = v;
@@ -16,6 +18,28 @@ void Init(int width, int height) {
 	camera.LookAt(Vector3(0.0f,0.0f,1.0f),Vector3(0.0f,0.0f,0.0f),Vector3(0.0f,1.0f,0.0f));
 	Ray ray = camera.GetRay(0.5f, 0.5f);
 }
+float GetEscaptedTime() {
+	static unsigned long sTimeSinceComputerStart = 0;
+	static unsigned long sLastFrameTime = 0;
+	sTimeSinceComputerStart = timeGetTime();
+	unsigned long frame_time = sLastFrameTime == 0 ? 0 : sTimeSinceComputerStart - sLastFrameTime;
+	sLastFrameTime = sTimeSinceComputerStart;
+	return float(frame_time) / 1000.0f;;
+}
+void RenderOnePixel(int pixel_index) {
+	int x = pixel_index % sViewportWidth;
+	int y = pixel_index / sViewportWidth;
+	SetColor(x, y, 255, 0, 255, 255);
+}
 void Render() {
-
+	static int sCurrentRenderedPixel = 0;
+	float current_render_time = 0.0f;
+	while (sCurrentRenderedPixel<sTotalPixelCount) {
+		RenderOnePixel(sCurrentRenderedPixel);
+		sCurrentRenderedPixel++;
+		current_render_time += GetEscaptedTime();
+		if (true || current_render_time > 0.0001f) {
+			break;
+		}
+	}
 }
